981bd8cd0deb33561f216a1fc8a766f092230313
kashinkoji <chandan18386@iiitd.ac.in>
2019-08-26 01:39:29 +0530

    initial commit

---
+++ shell.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<sys/wait.h> 
+#include<readline/readline.h> 
+#include<readline/history.h> 
+
+
+void clear() {
+    printf("\033[H\033[J");
+}
+
+void shell_init() {
+    clear();
+    printf("\n\n\n\n");
+    printf("Shell made by Chandan Gupta\n\n");
+    printf("Roll Number - 2018386\n");
+    printf("\n\n\n\n");
+    printf("User is @");
+    char* username = getenv("USER");
+    printf("%s\n\n",username);
+    sleep(10);
+}
+
+
+
+int main() {
+    shell_init();
+}
