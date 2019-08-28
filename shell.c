8687c17e2e924b34d8ca865ade9d105c19735b92
kashinkoji <chandan18386@iiitd.ac.in>
2019-08-28 19:08:59 +0530

    started again

--- shell.c
+++ shell.c
@@ -1,38 +1,14 @@
 #include<stdio.h>
-#include<stdio.h> 
-#include<string.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<sys/wait.h> 
-#include<readline/readline.h> 
-#include<readline/history.h> 
+#include<stdlib.h>
+#include<string.h>
 
-void clear() {
-    printf("\033[H\033[J");
+char *reading_command() {
+  char *command = NULL;
+  ssize_t buffsize = 0; 
+  getline(&command, &buffsize, stdin);
+  return command;
 }
 
-void current_path() {
-    char cwd[1024];
-    char* username = getenv("USER");
-    printf("%s:",username);
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("%s$\n", cwd);
-    }
-}
+void main() {
 
-void shell_init() {
-    clear();
-    printf("\n\n\n\n");
-    printf("Shell made by Chandan Gupta\n\n");
-    printf("Roll Number - 2018386\n");
-    printf("\n\n\n\n");
-    sleep(2);
-    clear();
-    current_path();
-}
-
-int main() {
-    shell_init();
-    
 }
