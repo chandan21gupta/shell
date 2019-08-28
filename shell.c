d464926d4e119e00b71d6f733e33147a82923174
kashinkoji <chandan18386@iiitd.ac.in>
2019-08-28 19:32:16 +0530

    added parser

--- shell.c
+++ shell.c
@@ -2,11 +2,30 @@
 #include<stdlib.h>
 #include<string.h>
 
+void clear() {
+  printf("\033[H\033[J");
+}
+
 char *reading_command() {
-  char *command = NULL;
   ssize_t buffsize = 0; 
+  char *command = NULL;
   getline(&command, &buffsize, stdin);
   return command;
+}
+
+char **parser(char *command) {
+  char **tokens = malloc(sizeof(command));
+  char *token;
+  
+  token = strtok(command," ");
+  int i = 0;
+  while(token) {
+    tokens[i] = token;
+    i++;
+    token = strtok(NULL," ");
+  }
+  tokens[i] = NULL;
+  return tokens;
 }
 
 void main() {
