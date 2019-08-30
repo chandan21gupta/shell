06f97e9c0ae1021ba048b71aca593ad36d159ad3
kashinkoji <chandan18386@iiitd.ac.in>
2019-08-30 16:05:38 +0530

    parser

--- shell.c
+++ shell.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>  
 
+//Only 100 commands in total are possible
 char *history[100];
 int command_number = 0;
 
 void add_history(char *command) {
-    history[command_number] = malloc(strlen(command)*sizeof(char));
-    history[command_number] = command;
-    command_number++;
+	if(strlen(command) != 0){
+	    history[command_number] = malloc(strlen(command)*sizeof(char));
+	    history[command_number] = command;
+	    command_number++;
+	}
 }
 
 void print_history() {
@@ -24,10 +28,25 @@ char *getCommand() {
     return command;
 }
 
+char **parser(char *command) {
+	int buffsize = 1024;
+	char **tokens = malloc(buffsize*sizeof(char));
+	char *token;
+	int i = 0;
+	token = strtok(command," ");
+	while(token != NULL) {
+		tokens[i] = token;
+		i++;
+		if(buffsize<=i) {
+			buffsize += 1024;
+			tokens = realloc(tokens,buffsize*sizeof(char));
+		}
+		token = strtok(NULL," ");
+	}
+	tokens[i] = NULL;
+	return tokens;
+}
+
 void main() {
-    for(int i=0;i<5;i++) {
-		char *command = getCommand();
-		add_history(command);    	
-    }
-    print_history();
+	
 }
