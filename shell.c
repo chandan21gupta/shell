e6278e7854119802847c1a804aa2c55ae42ea743
kashinkoji <chandan18386@iiitd.ac.in>
2019-08-30 14:58:05 +0530

    reinitialized

--- shell.c
+++ shell.c
@@ -2,32 +2,27 @@
 #include<stdlib.h>
 #include<string.h>
 
-void clear() {
-  printf("\033[H\033[J");
+char *history[100];
+int command_number = 0;
+
+void add_history(char *command) {
+    history[command_number] = malloc(strlen(command)*sizeof(char));
+    command_number++;
 }
 
-char *reading_command() {
-  ssize_t buffsize = 0; 
-  char *command = NULL;
-  getline(&command, &buffsize, stdin);
-  return command;
+void print_history() {
+    for(int i = 0;i<command_number;i++) {
+        printf("%s",history[i]);
+    }
 }
 
-char **parser(char *command) {
-  char **tokens = malloc(sizeof(command));
-  char *token;
-  
-  token = strtok(command," ");
-  int i = 0;
-  while(token) {
-    tokens[i] = token;
-    i++;
-    token = strtok(NULL," ");
-  }
-  tokens[i] = NULL;
-  return tokens;
+char *getCommand() {
+    char *command = NULL;
+    ssize_t buffsize = 0;
+    getline(&command,&buffsize,stdin);
+    return command;
 }
 
 void main() {
-
+    
 }
